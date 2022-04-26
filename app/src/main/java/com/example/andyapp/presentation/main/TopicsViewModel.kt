e3139d9a2aa26b83053c3086d7e5c248465f64dc
Enrique <jose.avalos@kavak.com>
2022-04-26 02:30:39 -0500

    add more questions to operaio

--- app/src/main/java/com/example/andyapp/presentation/main/TopicsViewModel.kt
+++ app/src/main/java/com/example/andyapp/presentation/main/TopicsViewModel.kt
@@ -41,6 +41,86 @@ class TopicsViewModel(private val topicRepository: QuestionsRepository) : ViewMo
                         )
                     ),
                     Quiz(
+                        question = "Pregunta enfocada a iniciativa",
+                        answer = "Hábleme de tres actividades que haya desempeñado por espacios largos de tiempo, que hayan requerido esfuerzo y disciplina",
+                        possibleAnswers = listOf(
+                            "Platíqueme de la última ocasión en que dijo una opinión diferente a la de su jefe",
+                            "¿Cuándo fue la última ocasión en que de forma circunstancial tuvo que tomar el control o dirigir un grupo?",
+                            "¿Recuerda alguna situación en la que haya convertido un problema en un reto o una oportunidad?",
+                            "Mencione un ejemplo de algo que haya hecho por encima de lo que su trabajo le exigía"
+                        )
+                    ),
+                    Quiz(
+                        question = "Pregunta enfocada a comunicación",
+                        answer = "Platícame de la última situación en que alguien se acercó a usted para platicarle o comentarle un asunto delicado",
+                        possibleAnswers = listOf(
+                            "Platícame de la última situación en que alguien se acercó a usted para platicarle o comentarle un asunto delicado",
+                            "¿Qué es para usted estar bien económicamente? ",
+                            "¿Qué busca para su familia? ",
+                            "¿Cómo le gusta ser reconocido?"
+                        )
+                    ),
+                    Quiz(
+                        question = "Pregunta enfocada a orientación a resultados",
+                        answer = "A la fecha ¿Cuál es el logro más significativo que ha alcanzado y que deseaba con anhelo",
+                        possibleAnswers = listOf(
+                            "¿Que aspectos lo tienen satisfecho en el plano familiar?",
+                            "¿En los últimos dos años, ¿Qué tanto se ha sentido reconocido en su vida y por qué?",
+                            "¿En los últimos dos años, ¿Qué tanto se ha sentido reconocido en su vida y por qué?",
+                            "A la fecha ¿Cuál es el logro más significativo que ha alcanzado y que deseaba con anhelo"
+                        )
+                    ),
+                    Quiz(
+                        question = "Pregunta enfocada a la experiencia",
+                        answer = "En tu antiguo puesto cual era la maquinaria que manejabas y como la aplicabas.",
+                        possibleAnswers = listOf(
+                            "Mencione un ejemplo de algo que haya hecho por encima de lo que su trabajo le exigía",
+                            "En tu antiguo puesto cual era la maquinaria que manejabas y como la aplicabas.",
+                            "¿Cuándo fue la última vez que experimentó la sensación de que parte de la información que usted maneja fue mal interpretada por otra persona?",
+                            "¿Recuerda haber tenido algún reconocimiento diferente al de sus compañeros por el cumplimiento de un trabajo?"
+                        )
+                    ),
+                    Quiz(
+                        question = "Pregunta enfocada a iniciativa ",
+                        answer = "Hábleme de tres actividades que haya desempeñado por espacios largos de tiempo, que hayan requerido esfuerzo y disciplina",
+                        possibleAnswers = listOf(
+                            "Platíqueme de la última ocasión en que dijo una opinión diferente a la de su jefe",
+                            "¿Cuándo fue la última ocasión en que de forma circunstancial tuvo que tomar el control o dirigir un grupo?",
+                            "¿Recuerda alguna situación en la que haya convertido un problema en un reto o una oportunidad?",
+                            "Mencione un ejemplo de algo que haya hecho por encima de lo que su trabajo le exigía"
+                        )
+                    ),
+                    Quiz(
+                        question = "Pregunta enfocada a iniciativa ",
+                        answer = "Hábleme de tres actividades que haya desempeñado por espacios largos de tiempo, que hayan requerido esfuerzo y disciplina",
+                        possibleAnswers = listOf(
+                            "Platíqueme de la última ocasión en que dijo una opinión diferente a la de su jefe",
+                            "¿Cuándo fue la última ocasión en que de forma circunstancial tuvo que tomar el control o dirigir un grupo?",
+                            "¿Recuerda alguna situación en la que haya convertido un problema en un reto o una oportunidad?",
+                            "Mencione un ejemplo de algo que haya hecho por encima de lo que su trabajo le exigía"
+                        )
+                    ),
+                    Quiz(
+                        question = "Pregunta enfocada a iniciativa ",
+                        answer = "Hábleme de tres actividades que haya desempeñado por espacios largos de tiempo, que hayan requerido esfuerzo y disciplina",
+                        possibleAnswers = listOf(
+                            "Platíqueme de la última ocasión en que dijo una opinión diferente a la de su jefe",
+                            "¿Cuándo fue la última ocasión en que de forma circunstancial tuvo que tomar el control o dirigir un grupo?",
+                            "¿Recuerda alguna situación en la que haya convertido un problema en un reto o una oportunidad?",
+                            "Mencione un ejemplo de algo que haya hecho por encima de lo que su trabajo le exigía"
+                        )
+                    ),
+                    Quiz(
+                        question = "Pregunta enfocada a iniciativa ",
+                        answer = "Hábleme de tres actividades que haya desempeñado por espacios largos de tiempo, que hayan requerido esfuerzo y disciplina",
+                        possibleAnswers = listOf(
+                            "Platíqueme de la última ocasión en que dijo una opinión diferente a la de su jefe",
+                            "¿Cuándo fue la última ocasión en que de forma circunstancial tuvo que tomar el control o dirigir un grupo?",
+                            "¿Recuerda alguna situación en la que haya convertido un problema en un reto o una oportunidad?",
+                            "Mencione un ejemplo de algo que haya hecho por encima de lo que su trabajo le exigía"
+                        )
+                    ),
+                    Quiz(
                         question = "Pregunta enfocada a iniciativa ",
                         answer = "Hábleme de tres actividades que haya desempeñado por espacios largos de tiempo, que hayan requerido esfuerzo y disciplina",
                         possibleAnswers = listOf(
